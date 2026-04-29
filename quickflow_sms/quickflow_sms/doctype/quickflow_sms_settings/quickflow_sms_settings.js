a42b7bbb9baed15cf6fe9a8b7f9085fccea753a7
solidad1234 <solidadkimeu@gmail.com>
2026-04-29 07:44:46 +0300

    implementing top up functionality

--- quickflow_sms/quickflow_sms/doctype/quickflow_sms_settings/quickflow_sms_settings.js
+++ quickflow_sms/quickflow_sms/doctype/quickflow_sms_settings/quickflow_sms_settings.js
@@ -1,8 +1,51 @@
 // Copyright (c) 2026, Solidad Kimeu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Quickflow SMS Settings", {
-// 	refresh(frm) {
+frappe.ui.form.on("Quickflow SMS Settings", {
+	top_up(frm) {
+		let d = new frappe.ui.Dialog({
+			title: __("Top Up"),
+			fields: [
+				{
+					label: __("Phone Number"),
+					fieldname: "mobile",
+					fieldtype: "Data",
+					reqd: 1,
+				},
+				{
+					label: __("Amount"),
+					fieldname: "amount",
+					fieldtype: "Currency",
+					reqd: 1,
+				},
+			],
+			primary_action_label: __("Top Up"),
+			primary_action(values) {
+				frappe.call({
+					method: "quickflow.services.payment.initiate_payment",
+					args: {
+						mobile: values.mobile,
+						amount: values.amount,
+					},
+					callback: function (r) {
+						if (r.message && r.message.ResponseCode === "0") {
+							frappe.msgprint(
+								__("STK Push initiated successfully. Please check your phone.")
+							);
+							d.hide();
+						} else if (r.message && r.message.CustomerMessage) {
+							frappe.msgprint({
+								title: __("Payment Status"),
+								message: r.message.CustomerMessage,
+								indicator: "blue",
+							});
+							d.hide();
+						}
+					},
+				});
+			},
+		});
 
-// 	},
-// });
+		d.show();
+	},
+});
